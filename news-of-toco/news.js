c511877349c0f948b8f755864182f9e8a8f9863e
LongHoang05 <clgamingtv69@gmail.com>
2025-11-04 23:51:00 +0700

    Delete news-of-toco directory

--- news-of-toco/news.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", () => {
-  const navbar = document.getElementById("header");
-  if (window.scrollY > 100) {
-    navbar.classList.add("scrolled");
-  } else {
-    navbar.classList.remove("scrolled");
-  }
-});
